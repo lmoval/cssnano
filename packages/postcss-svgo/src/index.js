998e92419b63bc650c63ce228b8e3fca5c2105a8
Luca Morelli <luca.morelli@ovalmoney.com>
2021-06-22 16:40:59 +0200

    feat: bump svgo to version 4.1.3

--- packages/postcss-svgo/src/index.js
+++ packages/postcss-svgo/src/index.js
@@ -1,6 +1,6 @@
 import postcss from 'postcss';
 import valueParser from 'postcss-value-parser';
-import SVGO from 'svgo';
+import { optimize } from 'svgo';
 import isSvg from 'is-svg';
 import { encode, decode } from './lib/url';
 
@@ -8,7 +8,7 @@ const PLUGIN = 'postcss-svgo';
 const dataURI = /data:image\/svg\+xml(;((charset=)?utf-8|base64))?,/i;
 const dataURIBase64 = /data:image\/svg\+xml;base64,/i;
 
-function minifyPromise(decl, getSvgo, opts) {
+function minifyPromise(decl, opts) {
   const promises = [];
   const parsed = valueParser(decl.value);
 
@@ -55,32 +55,32 @@ function minifyPromise(decl, getSvgo, opts) {
     }
 
     promises.push(
-      getSvgo()
-        .optimize(svg)
-        .then((result) => {
-          let data, optimizedValue;
+      new Promise((resolve) => {
+        resolve(optimize(svg, opts));
+      }).then((result) => {
+        let data, optimizedValue;
 
-          if (isBase64) {
-            data = Buffer.from(result.data).toString('base64');
-            optimizedValue = 'data:image/svg+xml;base64,' + data + url.hash;
-          } else {
-            data = isUriEncoded ? encode(result.data) : result.data;
-            // Should always encode # otherwise we yield a broken SVG
-            // in Firefox (works in Chrome however). See this issue:
-            // https://github.com/cssnano/cssnano/issues/245
-            data = data.replace(/#/g, '%23');
-            optimizedValue = 'data:image/svg+xml;charset=utf-8,' + data;
-            quote = isUriEncoded ? '"' : "'";
-          }
+        if (isBase64) {
+          data = Buffer.from(result.data).toString('base64');
+          optimizedValue = 'data:image/svg+xml;base64,' + data + url.hash;
+        } else {
+          data = isUriEncoded ? encode(result.data) : result.data;
+          // Should always encode # otherwise we yield a broken SVG
+          // in Firefox (works in Chrome however). See this issue:
+          // https://github.com/cssnano/cssnano/issues/245
+          data = data.replace(/#/g, '%23');
+          optimizedValue = 'data:image/svg+xml;charset=utf-8,' + data;
+          quote = isUriEncoded ? '"' : "'";
+        }
 
-          node.nodes[0] = Object.assign({}, node.nodes[0], {
-            value: optimizedValue,
-            quote: quote,
-            type: 'string',
-            before: '',
-            after: '',
-          });
-        })
+        node.nodes[0] = Object.assign({}, node.nodes[0], {
+          value: optimizedValue,
+          quote: quote,
+          type: 'string',
+          before: '',
+          after: '',
+        });
+      })
         .catch((error) => {
           throw new Error(`${PLUGIN}: ${error}`);
         })
@@ -93,16 +93,6 @@ function minifyPromise(decl, getSvgo, opts) {
 }
 
 export default postcss.plugin(PLUGIN, (opts = {}) => {
-  let svgo = null;
-
-  const getSvgo = () => {
-    if (!svgo) {
-      svgo = new SVGO(opts);
-    }
-
-    return svgo;
-  };
-
   return (css) => {
     return new Promise((resolve, reject) => {
       const svgoQueue = [];
@@ -112,7 +102,7 @@ export default postcss.plugin(PLUGIN, (opts = {}) => {
           return;
         }
 
-        svgoQueue.push(minifyPromise(decl, getSvgo, opts));
+        svgoQueue.push(minifyPromise(decl, opts));
       });
 
       return Promise.all(svgoQueue).then(resolve, reject);
